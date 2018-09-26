aa3b309206f3650bb2d52acf0802de7a4d2f8dbe
tangxy <1057742284@qq.com>
2018-09-26 14:17:07 +0700

    init

--- config/index.js
+++ config/index.js
@@ -33,7 +33,8 @@ module.exports = {
      */
 
     // https://webpack.js.org/configuration/devtool/#development
-    devtool: 'cheap-module-eval-source-map',
+    // devtool: 'cheap-module-eval-source-map',
+    devtool: 'source-map', // debug开启之前解开注释
 
     // If you have problems debugging vue-files in devtools,
     // set this to false - it *may* help
