63be7c81c09646790b837535c9a53e1792980b8c
tangxy <1057742284@qq.com>
2018-10-05 11:23:23 +0700

    update

---
+++ src/components/Render.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  render(h) {
+    return h('div',
+      {
+        class: {
+          succes: true,
+        },
+        style: {
+          color: 'red',
+          fontSize: '14px',
+        },
+        // 普通的 HTML 特性
+        attrs: {
+          id: 'foo',
+        },
+        ref: 'myRef',
+        // DOM 属性
+        domProps: {
+          innerHTML: '我在哪？快把我找出来！',
+        },
+        on: {
+          click: this.handleClick,
+        },
+      },
+      [
+        '头疼'
+      ],
+    );
+  },
+  methods: {
+    handleClick() {
+      console.log('是我！')
+    },
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Render from '@/components/Render';
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld,
+    },
+    {
+      path: '/render',
+      name: 'Render',
+      component: Render,
     },
   ],
 });
