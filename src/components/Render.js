a8ea24a86416265a053abeb954848018fec02d97
tangxy <1057742284@qq.com>
2018-10-05 17:57:25 +0700

    update

--- src/components/Render.js
+++ src/components/Render.js
@@ -1,39 +1,39 @@
-<script>
+// <script>
 export default {
   render(h) {
     return h(
-      "div",
+      'div',
       {
         class: {
-          succes: true
+          succes: true,
         },
         style: {
-          color: "red",
-          fontSize: "14px"
+          color: 'red',
+          fontSize: '14px',
         },
         // 普通的 HTML 特性
         attrs: {
-          id: "foo"
+          id: 'foo',
         },
-        ref: "myRef",
+        ref: 'myRef',
         // DOM 属性
         // domProps: {
         //   innerHTML: '我在哪？快把我找出来！',
         // },
         on: {
-          click: () => {this.handleClick(h)}
-        }
+          click: () => { this.handleClick(h); },
+        },
       },
       [
-        h("span", "我在哪？快把我找出来！"),
-        h("p", "ojbk!"),
+        h('span', '我在哪？快把我找出来！'),
+        h('p', 'ojbk!'),
       ],
     );
   },
   methods: {
     handleClick(e) {
       console.log(e);
-    }
-  }
+    },
+  },
 };
-</script>
+// </script>
