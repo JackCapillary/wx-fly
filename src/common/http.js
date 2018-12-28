3e506e882b98b1a56d0a4a799121931e3831efb8
tangxy <1057742284@qq.com>
2018-12-28 16:20:47 +0700

    update

--- src/common/http.js
+++ src/common/http.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import Qs from 'qs';
 // /* eslint-disable */
 // setCookie('token', '8c49c44e048d47701cca9b31a153fc67'); // 公测token打包时要注释掉
-// const baseURL = 'http://localhost:3000/'; // 本地环境
-const baseURL = 'http://tantanwin.com:3000/'; // 生产环境
+const baseURL = 'http://localhost:3000/'; // 本地环境
+// const baseURL = 'http://tantanwin.com:3000/'; // 生产环境
 const http = {
   post(repo, param) {
     const requestData = {
